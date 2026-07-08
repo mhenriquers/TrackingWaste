724e749a571b86d6a2b324535d9d2bb8f8ebac7d
Henrique <matheushenriqueowdail@gmail.com>
2026-07-08 01:27:04 -0300

    creating modalBills

--- Bills.tsx
+++ Bills.tsx
@@ -6,7 +6,18 @@ const Bills: React.FC = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
-        <Text>Contas</Text>
+        <View style={styles.card}>
+          <View style={styles.billcontainer}>
+            <View style={styles.textContainer}>
+              <View style={styles.titleContainer}>
+                <Text style={styles.textBill}>Internet</Text>
+              </View>
+              <Text> Venc: dia 5 </Text>
+            </View>
+            <View style={styles.checkMark}></View>
+          </View>
+        </View>
+
         <TouchableOpacity style={styles.triggercard}>
           <Text style={styles.triggertext}> + </Text>
         </TouchableOpacity>
@@ -18,9 +29,65 @@ const Bills: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "flex-start",
     alignItems: "center",
   },
+
+  card: {
+    borderWidth: 1,
+    borderColor: "#555",
+    borderRadius: 10,
+    minHeight: "11%",
+    width: "80%",
+    backgroundColor: "#fff",
+    elevation: 10,
+  },
+
+  billcontainer: {
+    minHeight: "6%",
+    width: "93%",
+    marginTop: 10,
+    marginHorizontal: 10,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    alignItems: "center",
+    alignContent: "center",
+    backgroundColor: "#8dd5ff",
+    borderWidth: 1,
+    borderColor: "#333",
+    borderRadius: 5,
+  },
+
+  textContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+
+    width: "82%",
+  },
+
+  titleContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    maxWidth: "65%",
+  },
+  textBill: {
+    fontSize: 20,
+    fontWeight: "bold",
+    margin: 10,
+  },
+
+  checkMark: {
+    height: 30,
+    width: 30,
+    borderWidth: 2,
+    borderRadius: 4,
+    borderColor: "#333",
+    backgroundColor: "#ffffff",
+    margin: 10,
+  },
+
   triggercard: {
     position: "absolute",
     bottom: 20,
---
+++ ModalBills.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import {
+  Modal,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  Keyboard,
+  KeyboardAvoidingView,
+} from "react-native";
